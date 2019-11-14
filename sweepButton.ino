6c4412e6b583aab967ba890554287a615181388e
jennifernj <jenniferneda@gmail.com>
2019-11-13 20:23:48 -0800

    Update sweepButton.ino

--- sweepButton.ino
+++ sweepButton.ino
@@ -1,49 +1,41 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo;
 
-const int buttonPin = 4; // number of the pushbutton pin
+const int buttonPin = 4;
 const int servoPin = 9;
-const int ledPin =  3; // number of the LED pin
+const int ledPin =  3;
 
-int buttonState = 0; // read pushbutton status
+int buttonState = 0;
 int pos = 0; // servo position
 int delayTime = 3000; // time for boba to come out
 
 void setup() {
-  // initialize the LED pin as an output
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input
   pinMode(buttonPin, INPUT);
-  // attach the servo on pin 9 to the servo object
-  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(servoPin);
 }
 
 void sweep() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);
+    delay(15);
   }
   delay(delayTime);
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(15);
   }
 }
 
 void loop() {
-  // read the state of the pushbutton value
   buttonState = digitalRead(buttonPin);
 
-  // if the button is pressed, the buttonState is HIGH
   if (buttonState == HIGH) {
-    // turn LED on
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, HIGH); // turn LED on if button pressed
     sweep();
   } else {
-    // turn LED off
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW); // turn LED off
   }
 
   
